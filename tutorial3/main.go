b5f0df454ee37333caac7de00cf4f8dc41d5f2b4
Joshua Hayes <joshhayes747@gmail.com>
2019-06-12 07:44:18 +1200

    Serve exercise 3 template on HTTP server

--- tutorial3/main.go
+++ tutorial3/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func check(msg string, err error) {
@@ -19,17 +21,28 @@ func main() {
 	check("Error opening file", err)
 	defer file.Close()
 
-	story := make(map[string]Arc)
+	story := make(map[string]arc)
 	err = json.NewDecoder(file).Decode(&story)
 	check("Error decoding json", err)
 
 	template, err := template.ParseFiles("template.html")
 	check("Error parsing template", err)
-	err = template.Execute(os.Stdout, story["intro"])
-	check("Error executing template", err)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		arc := strings.Split(r.RequestURI, "/")[1]
+		if arc == "" {
+			arc = "intro"
+		}
+
+		err = template.Execute(w, story[arc])
+		check("Error executing template", err)
+	})
+
+	http.ListenAndServe(":8080", mux)
 }
 
-type Arc struct {
+type arc struct {
 	Title   string
 	Story   []string
 	Options []struct {
